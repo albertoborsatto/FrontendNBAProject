c7072f31a361dbafa0382f12efd9ef6e1f48c60b
Alberto Vargas Borsatto <albertoborsatto@hotmail.com>
2025-08-14 16:11:48 -0300

    adding kd images

--- src/players.js
+++ src/players.js
@@ -1,10 +1,41 @@
 const urlBase = "https://api.balldontlie.io/v1";
 
+function setPlayerInfos(player) {
+    const playerInfo = {
+        PlayerFirstName: player.first_name,
+        PlayerLastName: player.last_name,
+        Position: player.position,
+        Height: player.height,  
+        Weight: player.weight,  
+        JerseyNumber: player.jersey_number
+    };
+    
+    const playerInfoList = document.createElement("ul");
+    playerInfoList.classList.add("players-info-list");
+    
+    Object.keys(playerInfo).forEach(key => {
+        const li = document.createElement("li");
+        li.textContent = `${key}: ${playerInfo[key]}`;
+        playerInfoList.appendChild(li);
+    });
+
+    return playerInfoList;
+}
+
 function renderPlayer(player) {
     const list = document.querySelector("#players-list");
-    const li = document.createElement("li");
-    li.textContent = `${player.first_name} ${player.last_name} - ${player.team.full_name}`;
-    list.appendChild(li);
+    const playerItem = document.createElement("li");
+    const playerImage = document.createElement("img");
+    playerImage.src = "https://cdn.nba.com/headshots/nba/latest/1040x760/201142.png";
+    playerItem.classList.add("player-list-item")
+    const playerDiv = document.createElement("div");
+    const playerInfo = setPlayerInfos(player);
+    playerDiv.classList.add("player-container");
+
+    playerDiv.appendChild(playerImage);
+    playerDiv.appendChild(playerInfo);
+    playerItem.appendChild(playerDiv);
+    list.appendChild(playerItem);
 }
 
 async function getPlayers() {
@@ -19,10 +50,13 @@ async function getPlayers() {
 
         if (!res.ok) throw new Error("Erro na API");
         const data = await res.json();
+        
         data.data.forEach(renderPlayer);
     } catch (err) {
         console.error(err);
     }
 }
 
-document.addEventListener("DOMContentLoaded", getPlayers);
+document.addEventListener("DOMContentLoaded", () => {
+    getPlayers();
+});
